81c5b8731ad26f800e00badc2c82b5f60178387f
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-10 09:48:21 -0700

    Add postUrl function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -11,3 +11,25 @@ export const getUrls = () => {
     return 'No links yet, go find some!'
   })
 }
+export const postUrl = (longUrl, title) => {
+  fetch('urlhere', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      "long_url": longUrl,
+      "title": title
+    })
+  })
+  .then(response => {
+    if (response.ok) {
+      return response
+    } else {
+      throw Error
+    }
+  })
+  .catch(error => {
+    alert('Oops! Something went wrong. Please try again!')
+  })
+}
