c3d1e56b5985044226a20bf74feb8c50532e045e
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-10 11:30:13 -0700

    Add conditional to render

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -2,20 +2,28 @@ import React from 'react';
 import './UrlContainer.css';
 
 const UrlContainer = props => {
-  const urlEls = props.urls.map(url => {
+  if(!props.urls) {
     return (
-      <div className="url">
-        <h3>{url.title}</h3>
-          <a href={url.short_url} target="blank">{url.short_url}</a>
-        <p>{url.long_url}</p>
-      </div>
+      <section data-testid="urlContainer" className="urlContainer">
+        <p>No urls yet! Find some to shorten!</p>
+      </section>
     )
-  });
-  return (
-    <section data-testid="urlContainer" className="urlContainer">
-      { urlEls ? urlEls : <p>No urls yet! Find some to shorten!</p> }
-    </section>
-  )
+  } else {
+    const urlEls = props.urls.map((url, index) => {
+      return (
+        <div key={index} className="url">
+        <h3>{url.title}</h3>
+        <a href={url.short_url} target="blank">{url.short_url}</a>
+        <p>{url.long_url}</p>
+        </div>
+      )
+    });
+    return (
+      <section data-testid="urlContainer" className="urlContainer">
+        {urlEls.length ? urlEls : <p>No urls yet! Find some to shorten!</p>}
+      </section>
+    )
+  }
 }
 
 export default UrlContainer;
