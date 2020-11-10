802db7441a6c9ee71c13084977377b8078a48fa4
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-10 11:49:51 -0700

    Add App Component Tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,8 @@ import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
 import App from './App.js';
-import { getUrls } from '../../apiCalls.js';
+import userEvent from '@testing-library/user-event';
+import { getUrls, postUrl } from '../../apiCalls.js';
 jest.mock('../../apiCalls.js');
 
 describe('App', () => {
@@ -43,5 +44,65 @@ describe('App', () => {
       expect(urlShort2).toBeInTheDocument()
     })
   })
-
+  describe('Integration tests', () => {
+    it('should render a Url Form', async () => {
+      getUrls.mockResolvedValue(
+        { urls: [{id: 1,
+        long_url: 'longUrl1',
+        short_url: 'shortUrl1',
+        title: 'title1'},
+        {id: 2,
+        long_url: 'longUrl2',
+        short_url: 'shortUrl2',
+        title: 'title2'}] }
+      )
+      render(
+        <App />
+      )
+      const header = screen.getByText('URL Shortener')
+      const titleInput = screen.getByPlaceholderText('Title...')
+      const urlInput = screen.getByPlaceholderText('URL to Shorten...')
+      const submitButton = screen.getByText('Shorten Please!')
+      expect(header).toBeInTheDocument()
+      expect(titleInput).toBeInTheDocument()
+      expect(urlInput).toBeInTheDocument()
+      expect(submitButton).toBeInTheDocument()
+    })
+    it('should render a Url Container', async () => {
+      getUrls.mockResolvedValue(
+        { urls: [{id: 1,
+        long_url: 'longUrl1',
+        short_url: 'shortUrl1',
+        title: 'title1'},
+        {id: 2,
+        long_url: 'longUrl2',
+        short_url: 'shortUrl2',
+        title: 'title2'}] }
+      )
+      render(
+        <App />
+      )
+      const header = screen.getByText('URL Shortener')
+      const containerWrap = screen.getByTestId('urlContainer')
+      expect(header).toBeInTheDocument()
+      expect(containerWrap).toBeInTheDocument()
+    })
+    it('should display a new url', async () => {
+      render(
+        <App />
+      )
+      const header = screen.getByText('URL Shortener')
+      const titleInput = screen.getByPlaceholderText('Title...')
+      const urlInput = screen.getByPlaceholderText('URL to Shorten...')
+      const submitButton = screen.getByText('Shorten Please!')
+      expect(header).toBeInTheDocument()
+      expect(titleInput).toBeInTheDocument()
+      expect(urlInput).toBeInTheDocument()
+      expect(submitButton).toBeInTheDocument()
+      userEvent.type(titleInput, 'test1')
+      userEvent.type(urlInput, 'test2')
+      userEvent.click(submitButton)
+      expect(postUrl).toHaveBeenCalledWith('test2', 'test1')
+    })
+  })
 })
